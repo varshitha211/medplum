c2a21143cc6972439987e52f0178f27a4a3adbca
varshitha211 <varshithaamara020@gmail.com>
2026-03-11 16:26:52 +0530

    Fix medplum config path

--- packages/server/start.js
+++ packages/server/start.js
@@ -1,9 +1,8 @@
+
 import WebSocket from "ws";
 
 globalThis.WebSocket = WebSocket;
 
-// correct path to root config
-process.env.MEDPLUM_CONFIG = "../../medplum_config.json";
+process.env.MEDPLUM_CONFIG = "/app/medplum_config.json";
 
-// start Medplum server
 import("./dist/index.js");
