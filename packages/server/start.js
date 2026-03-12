583c524d8ceee08276953d595879287757ced801
varshitha211 <varshithaamara020@gmail.com>
2026-03-12 09:50:09 +0530

    Add Medplum migrations before start

--- packages/server/start.js
+++ packages/server/start.js
@@ -1,8 +1,11 @@
-
-import WebSocket from "ws";
-
-globalThis.WebSocket = WebSocket;
+global.WebSocket = require("ws");
 
 process.env.MEDPLUM_CONFIG = "/app/medplum_config.json";
 
-import("./dist/index.js");
+const { execSync } = require("child_process");
+
+// Run database migrations
+execSync("node packages/server/dist/migrate.js", { stdio: "inherit" });
+
+// Start Medplum server
+require("./packages/server/dist/index.js");
