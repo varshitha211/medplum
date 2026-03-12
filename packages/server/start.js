de976fa8f52acf739517d767593f2fc7ab3784aa
varshitha211 <varshithaamara020@gmail.com>
2026-03-12 10:10:38 +0530

    Fix start.js

--- packages/server/start.js
+++ packages/server/start.js
@@ -1,12 +1,7 @@
 import WebSocket from "ws";
-import { execSync } from "child_process";
 
 globalThis.WebSocket = WebSocket;
 
 process.env.MEDPLUM_CONFIG = "/app/medplum_config.json";
 
-// Run database migrations
-execSync("node packages/server/dist/migrate.js", { stdio: "inherit" });
-
-// Start Medplum server
 await import("./packages/server/dist/index.js");
