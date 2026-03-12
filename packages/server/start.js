8775628a09fb070a6f7772266cc9a3fb5018602d
varshitha211 <varshithaamara020@gmail.com>
2026-03-12 11:13:23 +0530

    start server correctly

--- packages/server/start.js
+++ packages/server/start.js
@@ -1,10 +1,7 @@
 import WebSocket from "ws";
-import { spawn } from "child_process";
 
 globalThis.WebSocket = WebSocket;
 
 process.env.MEDPLUM_CONFIG = "/app/medplum_config.json";
 
-spawn("node", ["packages/server/dist/index.js"], {
-  stdio: "inherit"
-});
+await import("./dist/index.js");
